40646ddc94b572fac80553e5234e5413f2b3212b
Praseesh <praseeshprasee@gmail.com>
2024-04-17 12:21:08 +0530

    self

---
+++ Self.py
@@ -0,0 +1,15 @@
+class Sum:
+    def display(self):
+        print("This is display function")
+        
+a = Sum()
+a.display()
+
+"""
+Here no argument was pass through while calling display function,
+but,
+1 argument is passing to the function internally, 
+here the instance /object "a" is passing to while calling the method/function
+To represent that we are using self keyword
+ 
+"""
