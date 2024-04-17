39715b0cb2ad7b7f12f1623ef05e488fd269604a
Praseesh <praseeshprasee@gmail.com>
2024-04-17 11:40:07 +0530

    OOPS

---
+++ Class_and_Objects/Book .py
@@ -0,0 +1,16 @@
+
+
+class Book:
+    def __init__(self,) :
+
+        self.name = name
+        self.author = author
+        print(f"{self.name} is written by {self.author}")
+    def info(self):
+        print(f"{self.name} is written by {self.author}")
+# book1 = Book("The Da Vinci Code", "Dan Brown")
+name = "Cosmos"
+author = "Carl Sagan"
+book2 = Book()
+# book2.name = "Cosmos"
+# book2.author = "Carl Sagan"
---
+++ Class_and_Objects/Employee_basic.py
@@ -0,0 +1,13 @@
+    
+# Creating a class
+class Employee:
+    def work(self,name):
+        self.name = name
+        print(f"{self.name } is working")
+
+employee1 = Employee()
+employee1.work("amr")
+
+employee2 = Employee()
+employee2.work("ash")
+
