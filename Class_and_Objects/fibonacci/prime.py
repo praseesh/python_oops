3befc124e4ce4696e7f25237fefb51d87f111cd6
Praseesh <praseeshprasee@gmail.com>
2024-05-05 22:05:35 +0530

    prime

---
+++ Class_and_Objects/fibonacci/prime.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+# Example usage
+num = 17
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
