5b3cb0a686ae7dabe5ee818aaecd2d243136bb73
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-07-18 23:40:56 +0530

    Update Self.py

--- Self.py
+++ Self.py
@@ -1,13 +1,10 @@
 class Sum:
     def display(self):
         print("This is display function")
-        
 a = Sum()
 a.display()
 
 """
-
-
 Here no argument was pass through while calling display function,
 but,
 1 argument is passing to the function internally, 
