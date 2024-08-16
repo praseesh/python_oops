b706c578ff83294d5e912e0a0670b3957003a971
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-08-16 21:04:26 +0530

    Update Self.py

--- Self.py
+++ Self.py
@@ -4,13 +4,3 @@ class Sum:
 a = Sum()
 a.display()
 
-
-
-"""
-Here no argument was pass through while calling display function,
-but,
-1 argument is passing to the function internally, 
-here the instance /object "a" is passing to while calling the method/function
-To represent that we are using self keyword
- 
-"""
