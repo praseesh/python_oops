8ce5eb83ecaaa61769213c382654d5d1c1d4c214
Praseesh <praseeshprasee@gmail.com>
2024-08-24 19:57:37 +0530

    return book added

--- Uy_Lecture/library.py
+++ Uy_Lecture/library.py
@@ -6,19 +6,28 @@ class Library:
         for book in self.available_books:
             print("Name: ",book)
     
-    def lend_book(self):
-        pass
-    
+    def lend_book(self, book_search):
+        if self.book_search in self.available_books:
+            print(f"You borrowed ___{self.book_search}___")
+            self.available_books.remove(book_search)
+        else:
+            print("Book is not Available!!!!!....")
     def add_books(self):
         pass
     
 class Customer:
     def request_book(self):
-        pass
+        print("Enter the Book You Want: ")
+        self.book_search = input()
+        return self.book
     
     def return_book(self):
-        pass
+        print("Enter the name of the Book You Returning: ")
+        book_return = input()
+        return self.book_return
     
 library = Library(["Ikigai", "Atomic Habits", "You Can", "Secret"])
 library.display_available_books()
+
+customer = Customer()
 
